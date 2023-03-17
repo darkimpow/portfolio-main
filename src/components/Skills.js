83a77537168f44dc615f2998823aa5c98b4efed6
darkimpow <darkimpowell@gmail.com>
2023-03-17 18:41:00 -0400

    Created files for png's that will be used for skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
 
-function Skills(props) {
+function Skills() {
     return (
-        <div></div>
+        <div>Skills</div>
     );
 }
 
