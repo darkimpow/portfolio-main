d983c8a737514ff97f7d74cea29620932bb4bfae
darkimpow <darkimpowell@gmail.com>
2023-03-18 15:46:23 -0400

    spelling error

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
                 </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
-                        <p>Hi I'm Darkim, nice to meet you. Stay awhile,s it's cozy here.</p>
+                        <p>Hi I'm Darkim, nice to meet you. Stay awhile,it's cozy here.</p>
                     </div>
                     <div>
                         <p>
