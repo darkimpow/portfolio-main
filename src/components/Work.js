19fa2c05140dfa54b5279ab61ba8f972e3845ee4
darkimpow <darkimpowell@gmail.com>
2023-04-03 19:02:34 -0400

    updated work with new projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Aquarium from '../assets/projects/Aquarium.png'
 import CreativeP from '../assets/projects/CreativeP.png'
 import GitL from '../assets/projects/GitL.png'
+import TODO from '../assets/projects/TODO.png'
+import HuntAce from '../assets/projects/HuntAce.png'
 function Work() {
     return (
         <div name='work' className=' w-full md:h-screen text-gray-300 bg-slate-700'>
@@ -73,18 +75,18 @@ function Work() {
                     {/*More projects here*/}
 
                     {/*Card container*/}
-                    <div style={{background: ` url(${CreativeP})`}} className='shadow-lg shadow-slate-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div style={{background: ` url(${TODO})`}} className='shadow-lg shadow-slate-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/*Hover effect added here*/}
                         <div className='opacity-0 group-hover:opacity-100  '>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                CreativePainter.io
+                            <span className='text-2xl ml-8 font-bold text-white tracking-wider'>
+                                ToDo App
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="https://creative-painter-io-pearl.vercel.app/">
+                                <a href="https://todo-app-darkimpow.vercel.app/">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
                                 </a>
-                                <a href="https://github.com/darkimpow/CreativePainter.io">
+                                <a href="https://github.com/darkimpow/todo-app">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
                                 </a>
                             </div>
@@ -92,18 +94,18 @@ function Work() {
                     </div>
 
                     {/*Aquarium os*/}
-                    <div style={{background: ` url(${Aquarium})`}} className='shadow-lg shadow-slate-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    <div style={{background: ` url(${HuntAce})`}} className='shadow-lg shadow-slate-500 group container rounded-md flex justify-center items-center mx-auto content-div'>
 
                         {/*Hover effect added here*/}
                         <div className='opacity-0 group-hover:opacity-100  '>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                Aquarium.os
+                            <span className='text-2xl ml-3 font-bold text-white tracking-wider'>
+                                Hunt The Ace
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="https://deliverable-12-fish-tank-controller.vercel.app/main.html">
+                                <a href="https://hunt-the-ace-js-card-game.vercel.app/">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Demo</button>
                                 </a>
-                                <a href="https://github.com/darkimpow/Deliverable-12-Fish-Tank-Controller">
+                                <a href="https://github.com/darkimpow/Hunt-the-Ace-Js-card-game-">
                                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg '>Code</button>
                                 </a>
                             </div>
